1084968cd767ed5ac75bd8c905d20729492a77eb
Feodal0p <feodlaop@gmail.com>
2025-09-01 15:04:57 +0300

    laravel: AuthController register

--- social-map-laravel/app/Http/Controllers/AuthController.php
+++ social-map-laravel/app/Http/Controllers/AuthController.php
@@ -2,12 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserRegisterRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 
 class AuthController extends Controller
 {
-    public function register() {
-        return 'register';
+    public function register(UserRegisterRequest $request) : JsonResponse 
+    {
+        $data = $request->validated();
+        $data['role'] = User::ROLE_PARTICIPANT;
+        $user = User::create($data);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token,
+        ], 201);
     }
 
     public function login() {
