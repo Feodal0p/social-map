27757a1bfc6006d8d2d9f52f4af6086b60dd04af
Feodal0p <feodlaop@gmail.com>
2025-09-02 01:35:03 +0300

    react: minimal Register page

--- social-map-react/src/Pages/Register.jsx
+++ social-map-react/src/Pages/Register.jsx
@@ -1,8 +1,54 @@
+import axios from "../Plugin/axios"
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function Register() {
+
+    const navigate = useNavigate();
+
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        password_confirmation: ''
+    })
+
+    const [errors, setErrors] = useState({});
+
+    async function handeRegister(e) {
+        e.preventDefault()
+        await axios.post('/register', formData).catch(function (error) {
+            if (error.response) {
+                setErrors(error.response.data.errors);
+                console.log(error.response.data);
+            }
+        }).then(function (response) {
+            if (response && response.data) {
+                navigate('/profile');
+            }
+        })
+    }
 
     return (
         <>
-            <h1>Register Page</h1>
+            <form onSubmit={handeRegister}>
+                <input type="text" placeholder="Name" id="name"
+                    value={formData.name}
+                    onChange={(e) => setFormData({ ...formData, name: e.target.value })} />
+                {errors.name && <div className="error">{errors.name[0]}</div>}
+                <input type="text" placeholder="Email" id="email"
+                    value={formData.email}
+                    onChange={(e) => setFormData({ ...formData, email: e.target.value })} />
+                {errors.email && <div className="error">{errors.email[0]}</div>}
+                <input type="password" placeholder="Password" id="password"
+                    value={formData.password}
+                    onChange={(e) => setFormData({ ...formData, password: e.target.value })} />
+                {errors.password && <div className="error">{errors.password[0]}</div>}
+                <input type="password" placeholder="Password_confirmation" id="password_confirmation"
+                    value={formData.password_confirmation}
+                    onChange={(e) => setFormData({ ...formData, password_confirmation: e.target.value })} />
+                <button type="submit">Register</button>
+            </form>
         </>
     )
 }
