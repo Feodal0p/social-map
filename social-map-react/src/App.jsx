d7b9d1fdc71e0ef4ef3e85834c0b3f249ba12060
Feodal0p <feodlaop@gmail.com>
2025-09-02 18:24:32 +0300

    react: Header modify

--- social-map-react/src/App.jsx
+++ social-map-react/src/App.jsx
@@ -3,29 +3,31 @@ import './App.css'
 import Profile from './Pages/Profile.jsx'
 import Register from './Pages/Register.jsx'
 import Login from './Pages/Login.jsx'
+import Home from './Pages/Home.jsx'
 import ProtectedRoute from './Routes/ProtectedRoute.jsx'
 import GuestRoute from './Routes/GuestRoute.jsx'
+import MainLayout from './Layouts/MainLayout.jsx'
 
 export default function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/profile" element={
-          <ProtectedRoute>
-            <Profile/>
-          </ProtectedRoute>
-        } />
+        <Route element={<MainLayout />}>
+          <Route path="/profile" element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>} />
+          <Route index element={<Home />} />
+        </Route>
         <Route path="/register" element={
           <GuestRoute>
-            <Register/>
-          </GuestRoute>
-        } />
+            <Register />
+          </GuestRoute>} />
         <Route path="/login" element={
           <GuestRoute>
-            <Login/>
-          </GuestRoute>
-        } />
+            <Login />
+          </GuestRoute>} />
       </Routes>
     </BrowserRouter>
   )
--- social-map-react/src/Components/Header.jsx
+++ social-map-react/src/Components/Header.jsx
@@ -1,8 +1,29 @@
+import { useContext } from "react"
+import { Link } from "react-router-dom"
+import { AppContext } from "../Context/AppContext"
+
 export default function Header() {
 
+    const { user } = useContext(AppContext)
+
     return (
-        <header>
-            
+        <header className="header">
+            <nav className="nav">
+                <div className="nav-left">
+                    <span>LOGO</span>
+                    <Link to="/" className="nav-link">Social Map</Link>
+                </div>
+                {user ? (
+                    <div className="nav-right">
+                        <Link to="/profile" className="nav-link">Profile</Link>
+                    </div>
+                ) : (
+                    <div className="nav-right">
+                        <Link to="/login" className="nav-link">Login</Link>
+                        <Link to="/register" className="nav-link">Register</Link>
+                    </div>
+                )}
+            </nav>
         </header>
     )
 }
