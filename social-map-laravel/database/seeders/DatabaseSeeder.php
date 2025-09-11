19276047dd3a3e6f89b35262354f2c10f0970fb6
Feodal0p <feodlaop@gmail.com>
2025-09-11 17:42:14 +0300

    laravel: dbseeder

--- social-map-laravel/database/seeders/DatabaseSeeder.php
+++ social-map-laravel/database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
 use App\Models\User;
+use App\Models\Event;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,11 +15,40 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        Role::firstOrCreate(['name' => 'admin']);
+        Role::firstOrCreate(['name' => 'organizer']);
+        Role::firstOrCreate(['name' => 'participant']);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $user1 = User::firstOrCreate(
+            ['email' => '1@1'],
+            ['name' => 'Test User Organizer || Admin', 'password' => bcrypt('12345678')]
+        );
+        $user1->roles()->sync([Role::where('name', User::ROLE_ADMIN)->first()->id, Role::where('name', User::ROLE_ORGANIZER)->first()->id]);
+        $user1->profile()->create([
+            'avatar' => '/storage/images/user-default.png',
         ]);
+
+        $user2 = User::firstOrCreate(
+            ['email' => '2@2'],
+            ['name' => 'Test User Participant', 'password' => bcrypt('12345678')]
+        );
+        $user2->roles()->sync([Role::where('name', User::ROLE_PARTICIPANT)->first()->id]);
+        $user2->profile()->create([
+            'avatar' => '/storage/images/user-default.png',
+        ]);
+
+        Event::firstOrCreate(
+            ['title' => 'Test Event'],
+            [
+                'description' => 'This is the test event description.',
+                'start_time' => now()->addDays(5),
+                'end_time' => now()->addDays(10)->addHours(2),
+                'location' => 'вулиця Test 1488, Луцьк, Волинська область, Україна',
+                'latitude' => 50.747231,
+                'longitude' => 25.3196239,
+                'status' => Event::STATUS_UPCOMING,
+                'user_id' => $user1->id,
+            ]
+        );
     }
 }
