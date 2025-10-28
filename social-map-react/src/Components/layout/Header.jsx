a8f6f4f1f66f2e6fe46d3eb3a1a13df00c868301
Feodal0p <feodlaop@gmail.com>
2025-10-29 01:10:47 +0200

    react: redesign header

--- social-map-react/src/Components/layout/Header.jsx
+++ social-map-react/src/Components/layout/Header.jsx
@@ -1,6 +1,13 @@
 import { useContext, useState, useEffect } from "react"
 import { Link, useLocation } from "react-router-dom"
 import { AppContext } from "../../Context/AppContext"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+library.add(fas, far, fab);
 
 export default function Header() {
 
@@ -14,45 +21,45 @@ export default function Header() {
 
     return (
         <header className="header">
-            <nav className="nav">
-                <div className="nav-left">
-                    <Link to="/" className="nav-link">
-                        <span>LOGO</span>
-                        <span>Social Map</span>
-                    </Link>
-                </div>
-                {user ? (
-                    <div className="nav-right">
-                        <div className="nav-dropdown">
-                            <button
-                                onClick={() => setDropdownOpen(!dropdownOpen)}
-                                className="nav-link">Події</button>
-                            {dropdownOpen && (
-                                <div className="dropdown">
-                                    <Link to={`/events/participating`} className="nav-link">Беру участь</Link>
-                                    {user.roles.includes('organizer') && (
-                                        <Link to={`/events/created`} className="nav-link">Створені мною</Link>
-                                    )}
-                                    <Link to={`/events/history`} className="nav-link">Історія подій</Link>
-                                </div>
+            <Link to="/" className="nav-link">
+                <span>LOGO</span>
+                <span>Social Map</span>
+            </Link>
+            {user ? (
+                <nav>
+                    <div className="nav-dropdown">
+                        <button
+                            onClick={() => setDropdownOpen(!dropdownOpen)}
+                            className="nav-link">Події</button>
+                        <div className={`dropdown ${dropdownOpen ? 'open' : 'closed'}`}>
+                            <Link to={`/events/participating`} className="nav-link">
+                                <FontAwesomeIcon icon="fa-solid fa-circle-check" className="nav-link-icon" />
+                                Беру участь</Link>
+                            <Link to={`/events/history`} className="nav-link">
+                                <FontAwesomeIcon icon="fa-solid fa-clock-rotate-left" className="nav-link-icon" />
+                                Історія подій</Link>
+                            {user.roles.includes('organizer') && (
+                                <Link to={`/events/created`} className="nav-link">
+                                    <FontAwesomeIcon icon="fa-solid fa-pen-to-square" className="nav-link-icon" />
+                                    Створені мною</Link>
                             )}
                         </div>
-                        <Link to={`/profile/${user.profile_id}`} className="nav-link">
-                            <img
-                                src={user.profile_avatar}
-                                alt="Avatar"
-                                className="header-avatar"
-                            />
-                            <span>Profile</span>
-                        </Link>
                     </div>
-                ) : (
-                    <div className="nav-right">
-                        <Link to="/login" className="nav-link">Login</Link>
-                        <Link to="/register" className="nav-link">Register</Link>
-                    </div>
-                )}
-            </nav>
-        </header>
+                    <Link to={`/profile/${user.profile_id}`} className="nav-link with-profile">
+                        <img
+                            src={user.profile_avatar}
+                            alt="Avatar"
+                            className="header-avatar"
+                        />
+                        <span>Профіль</span>
+                    </Link>
+                </nav>
+            ) : (
+                <nav>
+                    <Link to="/login" className="nav-link">Login</Link>
+                    <Link to="/register" className="nav-link">Register</Link>
+                </nav>
+            )}
+        </header >
     )
 }
