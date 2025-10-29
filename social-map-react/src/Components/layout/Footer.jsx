203538ca65c0c79a7aa632db25f46b497bc6d7ca
Feodal0p <feodlaop@gmail.com>
2025-10-29 19:21:51 +0200

    react: redesign footer

--- social-map-react/src/Components/layout/Footer.jsx
+++ social-map-react/src/Components/layout/Footer.jsx
@@ -1,8 +1,17 @@
-export default function Header() {
+import { Link } from "react-router-dom";
+
+export default function Footer() {
 
     return (
         <footer>
-            
+            <p>© 2025 Платформа «Social Map». Всі права захищено.</p>
+            <div className="copyright">
+                <p>Геоданні: © <Link to={'https://www.openstreetmap.org/copyright'}> OpenStreetMap contributors</Link></p>
+                <span>|</span>
+                <p>Відображення: <Link to={'https://leafletjs.com/'}> Leaflet</Link></p>
+                <span>|</span>
+                <p>Геокодування: <Link to={'https://nominatim.org/'}>Nominatim</Link></p>
+            </div>
         </footer>
     )
 }
